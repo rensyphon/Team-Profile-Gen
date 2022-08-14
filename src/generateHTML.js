d0d357fce09ec5d77dc73c1f341b7695e30146b4
rensyphon <100047543+rensyphon@users.noreply.github.com>
2022-08-14 16:43:33 -0700

    added more HTML tags to be printed

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -8,7 +8,27 @@ function generateHTML(data) {
     <title>Document</title>
 </head>
 <body>
-    ${data.managerName}
+<div id="employeeCard">
+${data.managerName}<br>
+Manager
+ID: ${data.managerId}<br>
+Email: <a href="mailto:${data.managerEmail}">${data.managerEmail}</a><br>
+Office #: ${data.officeNumber}
+</div>
+    <div id="employeeCard">
+    ${data.engineerName}<br>
+    Engineer<br>
+    ID: ${data.engineerId}<br>
+    Email: <a href="mailto:${data.engineerEmail}">${data.engineerEmail}</a><br>
+    Github: <a href="https://www.github.com/${data.github}">https://www.github.com/${data.github}</a>
+    </div>
+    <div id="employeeCard">
+    ${data.internName}<br>
+    Intern<br>
+    ID: ${data.internId}<br>
+    Email: <a href="mailto:${data.internEmail}">${data.internEmail}</a><br>
+    School: ${data.school}
+    </div>
 </body>
 </html>
   `;
